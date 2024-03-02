4544c0dc35a6cc05d7792964b964663d39a140ef
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-03-02 18:55:13 +0200

    change errors

--- js_basic_task2/task2.js
+++ js_basic_task2/task2.js
@@ -8,42 +8,50 @@ function getRandomIntInclusive(min, max) {
 
   const basicPrice = 10000;
   let finalPrice = 0;
-  let temp = 0;
+  let priceAdjasment = 0;
   let odo = getRandomIntInclusive(0,200000);
   let age = getRandomIntInclusive(2000,2024);
-  let AC = getRandomIntInclusive(0,1);
-  let airBag = getRandomIntInclusive(0,1);
-  let electik = getRandomIntInclusive(0,1);
+  let hasAC = getRandomIntInclusive(0,1);
+  let hasAirBag = getRandomIntInclusive(0,1);
+  let isElectik = getRandomIntInclusive(0,1);
 
   if(age > 2019){
     
-     temp = basicPrice * 0.1;
-     finalPrice = basicPrice + temp;
+     priceAdjasment = basicPrice * 0.1;
+     finalPrice = basicPrice + priceAdjasment;
      
-      } else if(odo >= 50000 && odo <= 100000){
+      } 
+      
+  if(odo >= 50000 && odo <= 100000){
     
-         temp = basicPrice  * 0.05;
-         finalPrice = basicPrice - temp;
+         priceAdjasment = basicPrice  * 0.05;
+         finalPrice = basicPrice - priceAdjasment;
          
-        } else if(AC){
+        }
+        
+  if(hasAC){
     
-            temp = basicPrice * 0.07;
-            finalPrice = basicPrice + temp;
+            priceAdjasment = basicPrice * 0.07;
+            finalPrice = basicPrice + priceAdjasment;
             
-          } else if(airBag){
+          }
+          
+    if(hasAirBag){
     
-            temp = basicPrice * 0.08;
-            finalPrice = basicPrice + temp;
+            priceAdjasment = basicPrice * 0.08;
+            finalPrice = basicPrice + priceAdjasment;
             
-            } else if(electik){
+            }
+            
+    if(isElectik){
     
-                temp = basicPrice * 0.15;
-                finalPrice = basicPrice + temp;
+                priceAdjasment = basicPrice * 0.15;
+                finalPrice = basicPrice + priceAdjasment;
                 
   }
 //   console.log("age", age)
 //   console.log("odo", odo)
-//   console.log("AC", !!AC)
-//   console.log("airBag", !!airBag)
-//   console.log("electric", !!electik)
-  console.log("fialPrice is", chalk.bold.bgRed(finalPrice));
+//   console.log("hsaAC", !!hasAC)
+//   console.log("hasAirBag", !!hasAirBag)
+//   console.log("isElectric", !!isElectik)
+  console.log("finalPrice is", chalk.bold.bgRed(finalPrice));
---
+++ js_basic_task2/task3_1.js
@@ -0,0 +1,7 @@
+let arr = [6,2,1,8,10];
+
+let minArr = Math.min(arr);
+let maxArr = 0;
+
+
+console.log(minArr)
