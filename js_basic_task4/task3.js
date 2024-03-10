f069403e8cc57c1c8045b2d48b870ace58f77577
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-03-10 21:11:19 +0200

    edited task3

--- js_basic_task4/task3.js
+++ js_basic_task4/task3.js
@@ -1,3 +1,7 @@
+// Є масив ємейлів.
+// Використовуючи .filter(), запишіть у новий масив тільки ємейли, які містять 'gmail.com'.
+// Виведіть новий відфільтрований масив.
+
 const emails = ["example1@yahoo.com", "myemail@mail.com", "trueemail@gmail.com"];
 
 const filteredEmails = emails.filter(el => el.includes('gmail.com'));
