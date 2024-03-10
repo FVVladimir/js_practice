9fcf5f52688eef55ac13640a9b2b2302be3b8605
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-03-10 17:26:17 +0200

    task wos done

---
+++ js_basic_task4/task1.js
@@ -0,0 +1,20 @@
+import chalk from 'chalk';
+
+const numbers = [5, -4, 0, 6, 7, -1, 0, 0];
+
+let positiveNumbers = 0;
+let negativeNumbers = 0;
+let zeros = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+
+     if(numbers[i] > 0) {
+        positiveNumbers++;
+     } else if(numbers[i] < 0){
+        negativeNumbers++;
+     } else if(numbers[i] == 0){
+        zeros++;
+     }
+}
+
+console.log(`Positive numbers :${chalk.bgBlue(positiveNumbers)} \nNegative numbers :${chalk.bgBlue(negativeNumbers)} \nZeros :${chalk.bgBlue(zeros)}`)
