cb3af168f8265180e59d0a781025b115fc2c4bfb
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-03-10 21:14:07 +0200

    edited task4

--- js_basic_task4/task4.js
+++ js_basic_task4/task4.js
@@ -1,3 +1,6 @@
+// Є масив рядків імен.
+// Використовуючи .find(), виведіть перше ім'я, з довжиною більше 6 символів.
+
 const names = ["John", "Fernando", "Alexandra", "Anatolii", "Arina", "Elon"];
 
 const findedName = names.find(el => el.length > 6);
