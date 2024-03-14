f29a52cf044042d0dbf55e62be5b1a15ca4a87e3
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-03-14 19:46:15 +0200

    changed export

--- js_basic_task5/task3.js
+++ js_basic_task5/task3.js
@@ -1,8 +1,4 @@
-const arrSum = (arr) => {
-     
+export const arrSum = (arr) => {     
     return arr.reduce((acc,el) => acc + el)
-}
-
+};
 console.log(arrSum([12,34,67,11,"j"]));
-
-export default arrSum;
