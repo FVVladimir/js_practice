5445a75da0fd9952f8b0b4338bc59b0bc95078f3
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-03-14 19:46:28 +0200

    edit some errors

--- js_basic_task5/task4.js
+++ js_basic_task5/task4.js
@@ -1,17 +1,17 @@
-import  arrSum  from "./task3.js";
+import  {arrSum}  from "./task3.js";
 
-function averageGrade (...Grade) {
+function averageGrade (...grade) {
     
-    let averageGrade = arrSum(Grade);
+    let averageGrade = Math.floor(arrSum(grade) / grade.length);
     console.log(averageGrade)
 
-    if(averageGrade < 75) return console.log('Low performans');
-    if(averageGrade > 75 && averageGrade < 150) return console.log('Medium performans');
-    if(averageGrade > 150 && averageGrade < 250) return console.log('Good performans');
-    if(averageGrade > 250) return console.log('Very good performans');
+    if(averageGrade < 50) console.log('Low performans');
+    if(averageGrade > 50  && averageGrade < 69) console.log('Medium performans');
+    if(averageGrade > 70 && averageGrade < 89) console.log('Good performans');
+    if(averageGrade > 90) return console.log('Very good performans');
     
 }
 
 averageGrade(99,67,98);
-averageGrade(11,37,28);
+averageGrade(71,67,28);
 averageGrade(9,7,8);
