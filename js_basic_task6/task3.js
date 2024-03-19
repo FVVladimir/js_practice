03bcc4b9b1d3c8f22ac8bf825166d4de5c29a4d1
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-03-19 18:34:30 +0200

    task is done

--- js_basic_task6/task3.js
+++ js_basic_task6/task3.js
@@ -7,3 +7,36 @@
 // -Третій параметр повинен бути рядком та дорівнювати одному із 4 значень : +-/*
 // -Кількість аргументів не повинна бути більше 3-х
 // -Інші перевірки по бажанню
+
+function calculate (oper1, oper2, action) {
+
+        //   action.toString();
+            
+          switch(action) {
+
+            case '+':
+                return oper1 + oper2;
+                break;
+            case '-':
+                if (oper1 < oper2) {
+                    console.log('операнд 1 мае бути більше операнда 2')
+                } else return oper1 - oper2;
+                break;
+            case '*':
+                return oper1 * oper2;
+                break;
+            case '/':
+                if (oper2 == 0){
+                    console.log('на нуль ділити не можна')
+                }  else return oper1 / oper2;
+                break;
+                     
+           }           
+}
+
+console.log(calculate(3,5,'+'));
+console.log(calculate(3,5,'-'));
+console.log(calculate(5,3,'-'));
+console.log(calculate(14,2,'/'));
+console.log(calculate(3,0,'/'))
+console.log(calculate(3,6,'*'))
