97c905fb080565468235b9428400d024c40529fd
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-03-21 21:02:10 +0200

    task have done

---
+++ js_basic_task7/task1.js
@@ -0,0 +1,36 @@
+const phone = {
+    tradeMarck: "Nokia",
+    screeen: "5\"",
+    memory: "5GB",
+    call: function (){
+        console.log('please input namber')
+    },
+    break: function (){
+        console.log('call is over')
+    },
+};
+const car = {
+    marck: "kia",
+    seats: "5",
+    engen: "2.4",
+    start: function (){
+        return `Please check the full and start engen`
+    },
+    about: function (){
+        return `This car ${this.marck} have a ${this.seats} seats and engen ${this.engen} liter`
+    },
+};
+const person = {
+    name: "Johe",
+    age: "35",
+    edukation: "master",
+    sayHello: function (){
+        return `Hello ${this.name}`
+    },
+    askMe: function (){
+        return `Hello i have a degry ${this. edukation} you can ask me something`
+    },
+};
+phone.break();
+console.log(car.about());
+console.log(person.askMe());
